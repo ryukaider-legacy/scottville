4404383e4095e9e46a88d5a6678d24db74bd4db9
Scott Savino <ryukaider@gmail.com>
2012-05-17 00:47:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scottville::Application.config.secret_token = '4f9ab9ea308f870498c78b90fff11377d79d57522eea90eb300d22b29ad3f1587a70d25b89929f3d27aab71c8233b7dc79d4decd88a32426737141bd75e0d429'
