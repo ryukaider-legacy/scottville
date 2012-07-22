f936257993ffb22dded3e64974ad679bf5072e1e
Scott Savino <ryukaider@gmail.com>
2012-07-21 22:30:41 -0400

    trying to fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   include ApplicationHelper
-  default from: "from@example.com"
+  default from: "notifications@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
